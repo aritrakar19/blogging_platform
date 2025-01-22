4e2832e0dcdc897b2198a23f154c24280c9e9c9d
aritrakar19 <110339569+aritrakar19@users.noreply.github.com>
2025-01-22 17:18:34 +0530

    init

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const port = 5000;
 app.use(express.json()); // To parse JSON bodies
 app.use(cors()); // To handle CORS issues
 
-mongoose.connect("mongodb+srv://Aritrakar19:RNWjg.YAgCDwCy2@cluster0.4byyy.mongodb.net/")
+mongoose.connect("mongodb+srv://Aritrakar19:RNWjg.YAgCDwCy2@cluster0.4byyy.mongodb.net/test")
 
 // POST route for contact form
 app.post('/api/contact', (req, res) => {
